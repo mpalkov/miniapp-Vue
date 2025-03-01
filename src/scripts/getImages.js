6097d14f6963cf521094b6508aad949ad90eeebf
Martin Palkov <martin.palkov@gmail.com>
2025-03-01 10:37:07 +0100

    wip: cleanup code in getImages.js

--- src/scripts/getImages.js
+++ src/scripts/getImages.js
@@ -2,12 +2,9 @@ const API_URL_BASE = 'https://picsum.photos/v2/list';
 const IMAGES_FETCH_LIMIT = 50;
 
 export const getImages = async (pageNumber) => {
-  const started = Date.now();
-  
   const url = `${API_URL_BASE}/?page=${pageNumber}&limit=${IMAGES_FETCH_LIMIT}`
   const result = await fetch(url);
   const parsedResult = await result.json();
-  console.log('getimages duration', Date.now() - started, 'ms');
   
   return parsedResult;
 };
